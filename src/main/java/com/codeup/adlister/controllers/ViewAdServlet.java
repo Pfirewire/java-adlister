643f694272c2bfea16654673ac08018ae7c2711d
Stephen Pflug <stephen@pflug.us>
2022-09-10 16:45:19 -0500

    set up ViewAdServlet to display individual ad, no functionality yet

---
+++ src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
@@ -0,0 +1,22 @@
+package com.codeup.adlister.controllers;
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "ViewAdServlet", urlPatterns = "/ads/view")
+public class ViewAdServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long adId = Long.parseLong(request.getParameter("id"));
+
+    }
+}
